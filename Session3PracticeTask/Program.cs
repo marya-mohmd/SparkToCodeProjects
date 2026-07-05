c7960dc27f75f09de2f1c9c42a5606af69594a00
Marya <maryaalmaskari8@gmail.com>
2026-07-05 20:52:29 +0400

    Add project files.

---
+++ Session3PracticeTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session3PracticeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
