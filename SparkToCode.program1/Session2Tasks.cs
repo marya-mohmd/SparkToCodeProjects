ff8de28b5703978b1d4764db542b27fae88c3c66
Marya <maryaalmaskari8@gmail.com>
2026-07-05 05:29:57 +0400

    Solved Task 1: Countdown Timer

--- SparkToCode.program1/Session2Tasks.cs
+++ SparkToCode.program1/Session2Tasks.cs
@@ -6,5 +6,25 @@ namespace SparkToCode.program1
 {
     internal class Session2Tasks
     {
+        static void Main(string[] args)
+        {
+            //Task 1: Countdown Timer
+            Console.WriteLine("Please Enter a Starting Number:");
+            int StartingNumber = int.Parse(Console.ReadLine()); 
+            if (StartingNumber < 1) 
+            {
+                Console.WriteLine("Please Enter a Starting Number Higher Than 1:");
+            }
+            else
+            {
+                for(int y = StartingNumber; y >= 1; y--)
+                {
+                    Console.WriteLine(y);
+                }
+                Console.WriteLine("Liftoff!");
+            }
+
+            
     }
+}
 }
