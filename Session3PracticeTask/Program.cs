ba20d75a0922811b6383a7d3121ac13bf1ffef77
Marya <maryaalmaskari8@gmail.com>
2026-07-05 20:57:44 +0400

    Solved Task1: Absolute Difference

--- Session3PracticeTask/Program.cs
+++ Session3PracticeTask/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1: Absolute Difference
+            Console.WriteLine("Please enter the first number:");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            int SecondNumber = int.Parse(Console.ReadLine());
+            int AbsDiff = Math.Abs(SecondNumber - FirstNumber);
+
+            Console.WriteLine($"The absolute difference between {FirstNumber} and {SecondNumber} is: {AbsDiff}");
         }
     }
 }
