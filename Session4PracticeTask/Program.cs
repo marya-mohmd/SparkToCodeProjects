d04c2724137dd81ce42ddfd457a1bf730e752364
Marya <maryaalmaskari8@gmail.com>
2026-07-07 22:23:08 +0400

    Solved Task 1 - Personalized Welcome Function

---
+++ Session4PracticeTask/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Session4PracticeTask
+{
+    internal class Program
+    {
+        //Task 1 - Personalized Welcome Function
+        public static void PrintWelcomeMessage(string name)
+        {
+            Console.WriteLine("Welcome to Spark to Code, " + name);
+        }
+    }
+}
