cb3f00fae880cdc9a7161aad6b6b91a56812b57e
Marya <maryaalmaskari8@gmail.com>
2026-07-08 14:45:54 +0400

    Solved Task 4 - Fixed Menu Display Function

--- Session4PracticeTask/Program.cs
+++ Session4PracticeTask/Program.cs
@@ -13,15 +13,15 @@
             int square = number * number;
             return square;
         }
-            static void Main(string[] args)
-            {
-            Console.WriteLine("Enter a number:");
-            int userNumber = int.Parse(Console.ReadLine());
+        /*static void Main(string[] args)
+        {
+        Console.WriteLine("Enter a number:");
+        int userNumber = int.Parse(Console.ReadLine());
 
-            int squaredResult = Square(userNumber);
+        int squaredResult = Square(userNumber);
 
-            Console.WriteLine($"The square of {userNumber} is {squaredResult}");
-            }
+        Console.WriteLine($"The square of {userNumber} is {squaredResult}");
+        } */
 
         //Task 3 - Celsius to Fahrenheit Function
         public static double CelsiusToFahrenheit(double celsius)
@@ -29,12 +29,25 @@
             double fahrenheit = (celsius * 9 / 5) + 32;
             return fahrenheit;
         }
-        static void Main(string[] args)
+        /* static void Main(string[] args)
         {
             Console.WriteLine("Enter temperature in Celsius:");
             double celsius = double.Parse(Console.ReadLine());
             double fahrenheit = CelsiusToFahrenheit(celsius);
             Console.WriteLine(celsius + "=°C is equal to" + fahrenheit + "°F");
+        } */
+
+        //Task 4 - Fixed Menu Display Function
+        public static void DisplayMenu()
+        { 
+            Console.WriteLine("------Menu-------"); 
+            Console.WriteLine("1) Start");
+            Console.WriteLine("2) Help");
+            Console.WriteLine("3) Exit");
+        }
+        static void Main(string[] args)
+        {
+            DisplayMenu();
         }
 }
 }
