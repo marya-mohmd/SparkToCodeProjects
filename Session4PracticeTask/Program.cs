31a148364b45f07af1d85ae2a3f7a04dac0fa11c
Marya <maryaalmaskari8@gmail.com>
2026-07-08 12:16:25 +0400

    Solved Task 2 - Square Number Function

--- Session4PracticeTask/Program.cs
+++ Session4PracticeTask/Program.cs
@@ -7,5 +7,11 @@
         {
             Console.WriteLine("Welcome to Spark to Code, " + name);
         }
+        //Task 2 - Square Number Function
+        public static void Square(int number)
+        {
+            int square = number * number;
+            Console.WriteLine("The square of " + number + " is: " + square);
+        }
     }
 }
