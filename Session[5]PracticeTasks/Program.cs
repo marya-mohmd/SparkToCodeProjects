dc9eee9a021b063e976df3e8eb7c1248b8e126fe
Marya <maryaalmaskari8@gmail.com>
2026-07-08 21:14:15 +0400

    Solved Task 1 - Fixed Grades Array

---
+++ Session[5]PracticeTasks/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Session_5_PracticeTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1 - Fixed Grades Array
+            int[] grades = new int[5];
+            
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("Enter the grade of each subject:");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The grades are:");
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+
+            }
+        }
+    }
+}
