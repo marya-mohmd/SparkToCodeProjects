40228fd20118f1b9b8b868439f1604ae49eeb986
Marya <maryaalmaskari8@gmail.com>
2026-07-08 14:35:03 +0400

    Edit Tas 2

--- Session4PracticeTask/Program.cs
+++ Session4PracticeTask/Program.cs
@@ -8,10 +8,21 @@
             Console.WriteLine("Welcome to Spark to Code, " + name);
         }
         //Task 2 - Square Number Function
-        public static void Square(int number)
+        public static int Square(int number)
         {
             int square = number * number;
-            Console.WriteLine("The square of " + number + " is: " + square);
+            return square;
         }
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Enter a number:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            int squaredResult = Square(userNumber);
+
+            Console.WriteLine($"The square of {userNumber} is {squaredResult}");
+            }
+            
+        
     }
 }
