83feb1f9bb086d853ef2129f1173265dd462df1c
Marya <maryaalmaskari8@gmail.com>
2026-07-13 22:23:48 +0400

    Created class "BankAccount" with its Properties and Methods.

---
+++ OOP_Part1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OOP_Part1
+{
+    public class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string HolderName { get; set; }
+        public double Balance { get; set; }
+
+        public BankAccount() { }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            SendEmail();
+        }
+        public void withdraw(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+            }
+            SendEmail();
+        }
+        public double CheckBalance() 
+        {
+            PrintInformation();
+            return Balance; 
+        }
+        private void PrintInformation()
+        {
+            Console.WriteLine("Holder Name: " + HolderName);
+            Console.WriteLine("Balance: " + Balance);
+        }
+        private void SendEmail()
+        {
+            Console.WriteLine("Email notification sent to account holder.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
