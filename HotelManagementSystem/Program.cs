9cfc1f52272ffba10d25d4344d166cb8578b21d8
Marya <maryaalmaskari8@gmail.com>
2026-07-22 22:15:42 +0400

    Add Airline Database System ERD

---
+++ HotelManagementSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HotelManagementSystem
+{
+    class Room
+    {
+        public int roomNumber { get; }
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
+
+        public Room(int roomNumber, string roomType, double pricePerNight, bool isAvailable)
+        {
+            this.roomNumber = roomNumber;
+            this.roomType = roomType;
+            this.pricePerNight = pricePerNight;
+            this.isAvailable = isAvailable;
+        }
+
+        public void displayRoom()
+        {
+            Console.WriteLine($"Room Number   : {roomNumber}");
+            Console.WriteLine($"Room Type     : {roomType}");
+            Console.WriteLine($"Price / Night : OMR {pricePerNight}");
+            Console.WriteLine($"Status        : {(isAvailable ? "Available" : "Booked")}");
+        } 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
